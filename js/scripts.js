32b6a035f9ca950e836dcb169938bd843cea0b1b
Lex Wilkins <alexwilkins99@yahoo.com>
2019-03-05 21:16:54 -0800

    creates UI js that alerts the user to the number they type in the input field once the submit button has been clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
 var pingPong = function(number) {
   var num = parseInt(number);
-  console.log(num);
+  // console.log(num);
   return num;
 }
+
+$(document).ready(function(){
+  // debugger;
+  $('form#pingform').submit(function(event){
+    event.preventDefault();
+    var userNum = $('input#usernum').val();
+    // console.log(userNum);
+    var pingNum = pingPong(userNum);
+    // console.log(typeof pingNum);
+    alert(pingNum);
+
+  });
+
+
+});
